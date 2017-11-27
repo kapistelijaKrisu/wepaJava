554af63db84ab59ec33997a3243fde39e347ec2e
Krisu <krisuppi@yahoo.com>
2017-11-27 18:40:25 +0200

    maybe works?

--- src/main/java/wad/ProductionProfile.java
+++ src/main/java/wad/ProductionProfile.java
@@ -7,6 +7,7 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @Profile("production")
@@ -16,5 +17,13 @@ public class ProductionProfile {
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
         return (DataSource) DataSourceBuilder.create().build();
+    }
+    @Bean
+    public InternalResourceViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+
+        return resolver;
     }
 }
