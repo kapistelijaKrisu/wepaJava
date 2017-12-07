9bdaf750de2507651eed80e19abfc48569e6f38e
kapistelijaKrisu <krisuppi@yahoo.com>
2017-12-08 01:36:16 +0200

    milestone

--- src/main/java/wad/controller/ImageController.java
+++ src/main/java/wad/controller/ImageController.java
@@ -7,11 +7,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import wad.domain.News;
 import wad.repository.NewsRepository;
 
+@Transactional
 @Controller
 public class ImageController {
  
--- src/main/java/wad/controller/listNews/MainPageController.java
+++ src/main/java/wad/controller/listNews/MainPageController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import wad.domain.News;
@@ -14,6 +15,7 @@ import wad.repository.CategoryRepository;
 import wad.repository.NewsRepository;
 import wad.service.ViewInfoGenerator;
 
+@Transactional
 @Controller
 public class MainPageController {
     @Autowired
