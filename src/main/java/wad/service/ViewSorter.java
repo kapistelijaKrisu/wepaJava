ac1f8be0b01a0cc7fe973e47da8ca6a8fd373e51
kapistelijaKrisu <krisuppi@yahoo.com>
2017-12-08 00:41:09 +0200

    milestone

---
+++ src/main/java/wad/service/ViewSorter.java
@@ -0,0 +1,34 @@
+package wad.service;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import wad.domain.News;
+import wad.domain.View;
+
+@Service
+public class NewsSorter implements Comparator<View> {
+
+    public List<News> sortNewsByViews(List<View> views) {
+        views.sort(this);
+        List<News> news = new ArrayList<>();
+        views.forEach((view) -> {
+            news.add(view.getNews());
+        });
+        return news;
+    }
+
+    @Override
+    public int compare(View o1, View o2) {
+        int diff = (int) (o2.getViews() - o1.getViews());
+
+        if (diff == 0) {
+            diff = o2.compareTo(o1);
+
+        }
+        return diff;
+
+    }
+
+}
